861d21e4683f9f6302f72d3f575e6a0ec63088f9
Ian Benton <bentonian1@gmail.com>
2022-09-25 22:43:01 -0700

    db connect

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -10,8 +10,9 @@ export const serverSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET: z.string(),
   NEXTAUTH_URL: z.string().url(),
-  DISCORD_CLIENT_ID: z.string(),
-  DISCORD_CLIENT_SECRET: z.string(),
+  AUTH0_CLIENT_ID: z.string(),
+  AUTH0_CLIENT_SECRET: z.string(),
+  AUTH0_ISSUER: z.string(),
 });
 
 /**

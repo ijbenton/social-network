aab210051dfc8d662a83abab60b440b126396c56
Ian Benton <bentonian1@gmail.com>
2022-10-01 22:11:33 -0700

    navbar auth and unauth tests

--- src/components/Navbar/Navbar.test.tsx
+++ src/components/Navbar/Navbar.test.tsx
@@ -1,40 +1,48 @@
 import "@testing-library/jest-dom";
 
 import { Session } from "next-auth";
+import { useSession } from "next-auth/react";
 
 import { render, screen } from "../../utils/test-utils";
 import Navbar from "./Navbar";
 
-jest.mock("next-auth/react", () => {
-  const originalModule = jest.requireActual("next-auth/react");
-  const mockSession: Session = {
-    expires: new Date(Date.now() + 2 * 86400).toISOString(),
-    user: {
-      id: "123",
-      email: "hello@example.com",
-      image:
-        "https://lh3.googleusercontent.com/a-/ACNPEu9XMJU7P1gFZejccsoCGVijG4rPKBuu1zWxO5KJuQ=s96-c",
-      name: "ian",
-    },
-  };
-  return {
-    __esModule: true,
-    ...originalModule,
-    useSession: jest.fn(() => {
-      return { data: mockSession, status: "authenticated" }; // return type is [] in v3 but changed to {} in v4
-    }),
-  };
-});
+const mockAuthenticatedSession: Session = {
+  expires: new Date(Date.now() + 2 * 86400).toISOString(),
+  user: {
+    id: "123",
+    email: "hello@example.com",
+    image:
+      "https://lh3.googleusercontent.com/a-/ACNPEu9XMJU7P1gFZejccsoCGVijG4rPKBuu1zWxO5KJuQ=s96-c",
+    name: "ian",
+  },
+};
+
+jest.mock("next-auth/react");
 
 describe("Navbar", () => {
   it("renders correctly", () => {
     render(<Navbar />);
   });
 
-  it("displays user profile button", () => {
+  it("displays user profile button when signed in", () => {
+    (useSession as jest.Mock).mockReturnValueOnce({
+      data: mockAuthenticatedSession,
+      status: "authenticated",
+    });
     render(<Navbar />);
     const userMenuBtn = screen.getByTestId("user-menu-button");
 
     expect(userMenuBtn).toBeInTheDocument();
+  });
+
+  it("displays login text when signed out", () => {
+    (useSession as jest.Mock).mockReturnValueOnce({
+      data: {},
+      status: "unauthenticated",
+    });
+    render(<Navbar />);
+    const loginText = screen.getByText("Login");
+
+    expect(loginText).toBeInTheDocument();
   });
 });
